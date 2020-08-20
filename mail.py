e6463f8ffdecde6aabc23d8a79a56f86a77a6b6d
Sreejith S <67676040+sreejith8100@users.noreply.github.com>
2020-08-20 10:24:07 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,16 +2,10 @@ import smtplib, ssl
 
 port = 465
 smtp_server = "smtp.gmail.com"
-sender_email = "sreejith.sathiakumar@gmail.com"
-receiver_email = "sreelakshmi.sathyakumar@gmsil.com"
+sender_email = "me@gail.com" #sender's email id
+receiver_email = "you@gmail.com" #recipient's email id
 password = input("Type your password and press enter: ")
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
-    for x in range(0,100):
-        message = """\
-        Subject: DUMMI
-
-
-        """ + "DUMMI "*x
-        server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, message)
